74f7d0f5902efc6485d3e4c1fcd177e574111e31
Varun L <envy@Varuns-MacBook-Pro.local>
2022-08-11 21:51:31 +0530

    global package init and build

--- src/init.ts
+++ src/init.ts
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const {
     Connection,
     PublicKey,
@@ -29,7 +31,7 @@ let nodeWallet = {payer: keyPair};
 // let eventParser = new EventParser("5NbXrgnFeKfwpCPhpwFRrRZ8GqRCDpo94ohuHDBEPzdH", program.coder);
 
 const yaml = require('js-yaml');
-const inquirer = require("inquirer");
+const inquirer = require('inquirer');
 const fs = require('fs');
 const { stringify } = require("querystring");
 
@@ -217,60 +219,62 @@ function transformIDL(answers) {
     }
 }
 
-function generateProject(answers) {
+async function generateProject(answers) {
+    
 
     // Create Directory for the project
-    if(!fs.existsSync("../"+answers.INDEXER_NAME)){
-        fs.mkdirSync("../"+answers.INDEXER_NAME);
+    if(!fs.existsSync("./"+answers.INDEXER_NAME)){
+        fs.mkdirSync("./"+answers.INDEXER_NAME);
     }
 
     // Create YAML file inside the directory
-    fs.openSync("../"+answers.INDEXER_NAME+"/indexer.yaml", 'w');
+    fs.openSync("./"+answers.INDEXER_NAME+"/indexer.yaml", 'w');
 
     // Get the Data to be written into the YAML file
     let [data, eventHandlers] = transformIDL(answers);
-    console.log(data, eventHandlers);
     // Write into the YAML file
-    fs.writeFile("../"+answers.INDEXER_NAME+"/indexer.yaml", yaml.dump(data), (err) => {
+    fs.writeFileSync("./"+answers.INDEXER_NAME+"/indexer.yaml", yaml.dump(data), (err) => {
         if (err) {
             console.log(err);
         }
     });
 
     // Create IDL folder inside the directory
-    if(!fs.existsSync("../"+answers.INDEXER_NAME+"/idls")) {
-        fs.mkdirSync("../"+answers.INDEXER_NAME+"/idls");
+    if(!fs.existsSync("./"+answers.INDEXER_NAME+"/idls")) {
+        fs.mkdirSync("./"+answers.INDEXER_NAME+"/idls");
     }
 
     // Copy the IDL into the IDL folder
-    fs.copyFile(answers.PROGRAM_IDL,"../"+answers.INDEXER_NAME+"/idls/"+answers.PROGRAM_NAME+".json", (err) => {
+    fs.copyFile(answers.PROGRAM_IDL,"./"+answers.INDEXER_NAME+"/idls/"+answers.PROGRAM_NAME+".json", (err) => {
         if (err) throw err;
     });
 
     // Create SRC folder inside the directory
-    if(!fs.existsSync("../"+answers.INDEXER_NAME+"/src")) {
-        fs.mkdirSync("../"+answers.INDEXER_NAME+"/src");
+    if(!fs.existsSync("./"+answers.INDEXER_NAME+"/src")) {
+        fs.mkdirSync("./"+answers.INDEXER_NAME+"/src");
     }
 
     // Create mapping.js file inside the directory
-    // fs.openSync("../"+answers.INDEXER_NAME+"/src/mappings.js", 'w');
+    // fs.openSync("./"+answers.INDEXER_NAME+"/src/mappings.js", 'w');
     createEntitiesYaml(answers);
-    createMapping(answers,data.dataSources[0].mapping.eventHandlers);
-    if(!fs.existsSync("../"+answers.INDEXER_NAME+"/package")) {
-        fs.mkdirSync("../"+answers.INDEXER_NAME+"/package");
+    createMapping(answers,data["dataSources"][0].mapping.eventHandlers);
+    if(!fs.existsSync("./"+answers.INDEXER_NAME+"/package")) {
+        fs.mkdirSync("./"+answers.INDEXER_NAME+"/package");
     }
-    fs.openSync("../"+answers.INDEXER_NAME+"/package/generator.ts", 'w');
-    fs.copyFileSync("./generator.ts", "../"+answers.INDEXER_NAME+"/package/generator.ts");
+    fs.openSync("./"+answers.INDEXER_NAME+"/package/generator.ts", 'w');
+    fs.copyFileSync(__dirname + "/generator.ts", "./"+answers.INDEXER_NAME+"/package/generator.ts");
+    await createNPM(answers);
+    await createTSconfig(answers);
+    await installPackages(answers);
 }
 
 function createMapping(answers,eventHandlers) {
 
-    var logger = fs.createWriteStream('../'+answers.INDEXER_NAME+'/src/mapping.ts', {
+    var logger = fs.createWriteStream('./'+answers.INDEXER_NAME+'/src/mapping.ts', {
         flags: 'a'
     })
     
     let eventList = eventHandlers.map((event) => {
-        console.log(event);
         return parseEventString(event.event, event.handler);
     })
 
@@ -290,7 +294,7 @@ function createMapping(answers,eventHandlers) {
     
 }
 
-const createYAML = async() => {
+export async function createYAML() {
     const answers = await indexerInputs();
     if(checkProgram(answers.PROGRAM_ID, answers.NETWORK)) {
         if(validateFile(answers.PROGRAM_IDL)){
@@ -305,7 +309,7 @@ const createYAML = async() => {
 }
 
 function createEntitiesYaml(answers) {
-    fs.writeFile("../"+answers.INDEXER_NAME+"/entities.yaml" , yaml.dump({
+    fs.writeFileSync("./"+answers.INDEXER_NAME+"/entities.yaml" , yaml.dump({
         "entities": [
             {
                 "name" : "SampleEntity",
@@ -330,12 +334,53 @@ function createEntitiesYaml(answers) {
     })
 }
 
-async function createNPM() {
-    const output = execSync('cd ..', { encoding: 'utf-8' });
-
-    console.log('The output is:');
-    console.log(output);
+async function createNPM(answers) {
+    fs.openSync("./"+answers.INDEXER_NAME+"/package.json", 'w');
+    fs.writeFileSync("./"+answers.INDEXER_NAME+"/package.json", JSON.stringify({
+            "name": answers.INDEXER_NAME,
+            "version": "1.0.0",
+            "description": "",
+            "main": "index.js",
+            "scripts": {
+              "build": "grapher build",
+            },
+            "author": "",
+            "license": "ISC",
+            "dependencies": {
+              "@project-serum/anchor": "^0.25.0",
+              "@types/node": "^18.6.5",
+              "ts-node": "^10.9.1",
+            }
+    },null,2), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 }
 
-createYAML();
-// createNPM();
+async function createTSconfig(answers) {
+    fs.openSync("./"+answers.INDEXER_NAME+"/tsconfig.json", 'w');
+    fs.writeFileSync("./"+answers.INDEXER_NAME+"/tsconfig.json", JSON.stringify({
+        "compilerOptions": {
+        "module": "commonjs",
+        "declaration": true,
+        "removeComments": true,
+        "allowSyntheticDefaultImports": true,
+        "target": "es2017",
+        "sourceMap": false,
+        "outDir": "./dist",
+        "baseUrl": "./",
+        "incremental": true
+        },
+        "include": ["src/**/*.ts"],
+        "exclude": ["node modules","test","lib", "**/*spec.ts"]
+      },null,2), (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
+}
+
+function installPackages(answers) {
+    execSync("cd "+answers.INDEXER_NAME+" && npm i");
+}
